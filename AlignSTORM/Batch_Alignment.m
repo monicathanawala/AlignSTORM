bb49e0586739917d9df57202fd062122e01f44a2
monicathanawala <monicathanawala@users.noreply.github.com>
2019-04-25 14:46:33 -0400

    Update comments in BatchAlignment.m

--- AlignSTORM/Batch_Alignment.m
+++ AlignSTORM/Batch_Alignment.m
@@ -1,8 +1,8 @@
 function [filePath, filePath_STORM, d] = Batch_Alignment(drive, expt, varargin);
-%drive = 'Z:';
-%expt = '2018_09_25_A';
-%cd 'D:\2019_01_17_A'
-%cd '\\Nuc\d\2018_09_25_A'
+%drive = 'Z:'; the drive where your data is
+%expt = '2018_09_25_A'; or the folder you have all the data in
+%varargin = the subfolders which contain each round of imaging data. i.e., 'round1', 'round2', 'round3',... As it is
+%now, the data must be organized with each round in a different folder
 
 
 cd 'C:\Users\Monica\Dropbox\MATLAB' %use this for windows
@@ -10,10 +10,10 @@ cd 'C:\Users\Monica\Dropbox\MATLAB' %use this for windows
 
 for i=1:nargin-2;
     mypath = fullfile(drive, expt, varargin{i}, '/');
-    dir(fullfile(mypath, 'bead488*.bin'))
+    dir(fullfile(mypath, 'bead488*.bin')) %modify to identify your fiducial bead images
     beadfileList = dir(fullfile(mypath, 'bead488*.bin'))
     dir(fullfile(mypath, 'bead488*.bin'))
-    STORMfileList = dir(fullfile(mypath, 'STORM647*list_dc.bin'));
+    STORMfileList = dir(fullfile(mypath, 'STORM647*list_dc.bin')); %modify to find your STORM images
 
     for i2=1:max(size(beadfileList));
         filePath{i2,i} = fullfile(mypath, beadfileList(i2).name);
@@ -22,8 +22,8 @@ for i=1:nargin-2;
     
 end
 
-cd 'C:\Users\Monica\Dropbox\MATLAB\AlignSTORM' %windows
-%cd '/Users/Monica/Dropbox/MATLAB/AlignSTORM' %mac
+cd 'C:\Users\Monica\Dropbox\MATLAB\AlignSTORM' %windows path
+%cd '/Users/Monica/Dropbox/MATLAB/AlignSTORM' %mac path
 BinaryBeadImage;
 %this serves to get x and y shifts for all images and get affine transformation on bead images
 
